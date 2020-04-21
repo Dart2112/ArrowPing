37c46e769b5883cf06203f24cac521a3f5ebe2fe
Benjamin <DNAblue2112@gmail.com>
2020-04-21 13:53:26 +0930

    Added code comments and fixed it so that the code actually does something

--- src/main/java/net/lapismc/arrowping/ArrowPingListener.java
+++ src/main/java/net/lapismc/arrowping/ArrowPingListener.java
@@ -16,18 +16,28 @@ public class ArrowPingListener implements Listener {
 
     @EventHandler
     public void arrowHitEvent(EntityDamageByEntityEvent e){
-        if (!(e.getEntity() instanceof Player)) {
+        //Check if a player has been shot
+        if (e.getEntity() instanceof Player) {
+            //Ignore since hitting a player already makes a sound
             return;
         }
+        //Check if the hit was from an arrow
         if (!(e.getDamager() instanceof Arrow)) {
+            //Ignore if its not an arrow
             return;
         }
+        //Check if the thing that shot the arrow was a player
         if (!(((Arrow) e.getDamager()).getShooter() instanceof Player)) {
+            //TODO: maybe play a sound at the location of the shooter, e.g. if a skeleton hits someone you can hear a ding come from the skeleton
             return;
         }
+        //Get the player who shot the arrow
         Player p = (Player) ((Arrow) e.getDamager()).getShooter();
-        if(p == null)
+        //Make sure the player is online, theoretically the player could leave after an arrow is shot and before it lands?
+        //Maybe not, best to null check anyways
+        if (p == null)
             return;
+        //Play a sound to the player to signify that they hit an entity
         p.playSound(p.getLocation(), XSound.ENTITY_ARROW_HIT_PLAYER.parseSound(), 1f, 1f);
     }
 
